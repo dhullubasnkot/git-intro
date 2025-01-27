2df9f2b6372d48c4c50c73dddb609834894bd879
dhullubasnkot <123baglung@gmail.com>
2025-01-27 12:10:37 +0545

    added about.js

---
+++ about.js
@@ -0,0 +1,13 @@
+// about.js
+
+// This file contains information about the application
+
+const appInfo = {
+    name: "Git Intro",
+    version: "1.0.0",
+    description: "A simple application to introduce Git concepts",
+    author: "Your Name",
+    license: "MIT"
+};
+
+module.exports = appInfo;
